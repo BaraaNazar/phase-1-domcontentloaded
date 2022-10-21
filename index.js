e742faa7e31fa7c07dd3c1743de4267806307463
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-10-21 15:32:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const paragh = document.getElementById("text")
+    paragh.addEventListener("click",()=>{
+        paragh.innerHTML = "This is really cool!"
+    });
+    paragh.innerHTML = "This is really cool!"
+
+    });
